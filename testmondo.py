36f46919262cb82bccb1df1075ba1fb0fcdd5174
merlinxcy <xcy_merlin@outlook.com>
2017-06-28 14:46:35 +0800

    mongo

---
+++ testmondo.py
@@ -0,0 +1,11 @@
+import mongolib
+class a():
+	def aa(self):
+		a=mongolib.mongodb()
+		a.log_collect(msg='1gaejiusfuadaifuagusuifhiau afdu gaudf uisg uagsi gaug asyaigasydg aug iug ')
+		a.log_collect(msg='2')
+		a.log_input()
+		a.log_output()
+
+aaaa=a()
+aaaa.aa()
